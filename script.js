660bd85c407a7039f8b7d31a4aec203a4e653447
yeapzj0517 <yeapzj0517@gmail.com>
2025-12-30 00:07:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const images = [
+  "img/img 1.jpg",
+  "img/img 2.jpg",
+  "img/img 3.jpg",
+  "img/img 4.jpg"
+];
+
+let currentIndex = 0;
+const carouselImage = document.getElementById("carouselImage");
+
+function showImage(index) {
+  carouselImage.src = images[index];
+}
+
+function nextImage() {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  showImage(currentIndex);
+}
+
+let slideInterval = setInterval(nextImage, 3000);
+
+$(document).ready(function () {
+  $(".gallery h2").hide().fadeIn(1500);
+});
+
+$(".carousel").hover(
+  function () {
+    clearInterval(slideInterval);
+  },
+  function () {
+    slideInterval = setInterval(nextImage, 3000);
+  }
+);
